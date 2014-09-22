9c5cd19582d4f7786d139f35f2b493e9890a965a
Harsh Pandey <hpandey793@gmail.com>
2014-09-22 14:22:46 -0400

    removed console.log's

--- server/server.js
+++ server/server.js
@@ -5,10 +5,8 @@ Meteor.publish('users', function () {
 Meteor.publish('queue', function (roomId) {
   var userId = this.userId, connectionId = this.connection.id;
   if (userId) {
-    console.log('publish ' + roomId + ' ' + connectionId);
     Meteor.users.update(userId, {$addToSet: {rooms: {room: roomId, session: connectionId}}});
     this.onStop(function () {
-      console.log('unpublish ' + roomId + ' ' + connectionId);
       Meteor.users.update(userId, {$pull: {rooms: {room: roomId, session: connectionId}}});
     });
     return Songs.find({addedFrom: roomId});
