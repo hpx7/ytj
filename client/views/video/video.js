a3ab389fc96941c440e3bb828440f8b96908903a
Harsh Pandey <hpandey793@gmail.com>
2014-10-07 03:09:04 -0400

    simplified video.js

--- client/views/video/video.js
+++ client/views/video/video.js
@@ -1,4 +1,4 @@
-var player = null;
+var player;
 
 onYouTubeIframeAPIReady = initPlayer;
 
@@ -7,30 +7,26 @@ function songEnd () {
   if (Songs.find({addedFrom: Session.get('roomId')}).count() === 1) {
     var r = Math.random() * 16 | 0;
     Meteor.call('addSong', RelatedSongs.find().fetch()[r], Session.get('roomId'));
-    Meteor.call('removeSong', song._id, Session.get('roomId'));
-  } else
-    if (song) Meteor.call('removeSong', song._id, Session.get('roomId'));
+  }
+  if (song) Meteor.call('removeSong', song._id, Session.get('roomId'));
 }
 
 function initPlayer () {
-  try {
-    player = new YT.Player('player', {
-      events: {
-        'onStateChange': function (e) {
-          if (e.data === YT.PlayerState.ENDED)
-            songEnd();
-        },
-        'onReady': playSong
+  player = typeof YT !== 'undefined' && new YT.Player('player', {
+    events: {
+      'onStateChange': function (e) {
+        if (e.data === YT.PlayerState.ENDED) songEnd();
       },
-      playerVars: {'rel': 0}
-    });
-  } catch (err) {}
+      'onReady': playSong
+    },
+    playerVars: {'rel': 0}
+  });
 }
 
 function playSong () {
   var song = Songs.findOne({addedFrom: Session.get('roomId')}, {sort: {addedAt: 1}});
   try {
-    if (song) player.loadVideoById(song.yt_id);
+    if (song && player) player.loadVideoById(song.yt_id);
   } catch (err) {}
 }
 
