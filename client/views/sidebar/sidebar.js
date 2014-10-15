68d9c7d822bc4d141abad23647fa2ce6ffce297a
Harsh Pandey <hpandey793@gmail.com>
2014-10-15 00:37:29 -0400

    removed tab links hack

--- client/views/sidebar/sidebar.js
+++ client/views/sidebar/sidebar.js
@@ -16,10 +16,5 @@ Template.sidebar.events({
   'click .song': function (e) {
   	$(e.target).closest('.song').fadeOut(100).fadeIn(100);
     Meteor.call('addSong', this, Session.get('roomId'), handleError);
-  },
-  'click a': function (e) {
-    Session.set('tab', e.target.text);
-    IronLocation.pushState(null, 'YouTube Jukebox', e.target.href, true);
-    return false;
   }
 });
