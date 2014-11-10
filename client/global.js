7573aeb0ac1db66a19734cd764304497ddf0a46e
Harsh Pandey <hpandey793@gmail.com>
2014-11-10 15:48:42 -0500

    added template specific loading

--- client/global.js
+++ client/global.js
@@ -1,3 +1,7 @@
 handleError = function (error) {
   if (error) alert('Error: ' + error.message);
 };
+
+Template.registerHelper('handleReady', function (handleName) {
+  return Router.current() && Router.current()[handleName].ready()
+});
