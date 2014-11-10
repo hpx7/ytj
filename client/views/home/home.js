cc8251c810951f99dc922a81bedcd9baec14166d
Harsh Pandey <hpandey793@gmail.com>
2014-11-10 15:18:34 -0500

    sort rooms alphabetically

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,6 +1,6 @@
 Template.home.helpers({
   rooms: function () {
-    return Rooms.find();
+    return Rooms.find({}, {sort: {ownerName: 1}});
   },
   numListeners: function () {
     return Listeners.find({roomId: this._id, userId: {$ne: null}}).count();
