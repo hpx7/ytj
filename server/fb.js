747d69b6f60a541ea424aa406d56bda213d29361
Harsh Pandey <hpandey793@gmail.com>
2014-11-18 17:31:05 -0500

    improved publications

--- server/fb.js
+++ server/fb.js
@@ -1,8 +1,9 @@
-getFriendIds = function (userId) {
+friendIds = function (userId) {
+  if (!userId) return []
   var fbInfo = Meteor.users.findOne(userId).services.facebook;
   return _.pluck(HTTP.get('https://graph.facebook.com/v2.2/' + fbInfo.id + '/friends', {
     params: {access_token: fbInfo.accessToken, limit: 1000}
-  }).data.data, 'id');
+  }).data.data, 'id').concat(fbInfo.id);
 };
 
 friends = function (userId, fbId) {
--- server/server.js
+++ server/server.js
@@ -1,30 +1,37 @@
 roomVisible = function (userId, roomId) {
   var room = Rooms.findOne(roomId);
-  return room && (room.ownerId === userId || friends(userId, room.ownerFbId));
-}
+  return userId && room && (room.ownerId === userId || friends(userId, room.ownerFbId));
+};
 
 Meteor.publish(null, function () {
   return [Meteor.users.find(this.userId, {fields: {name: 1}}), Rooms.find({ownerId: this.userId})];
 });
 
-Meteor.publish('rooms', function (roomId) {
-  var friendIds = this.userId ? getFriendIds(this.userId) : [];
-  return roomId ? Rooms.find({_id: roomId, ownerFbId: {$in: friendIds}}) : Rooms.find({ownerFbId: {$in: friendIds}});
+Meteor.publish('rooms', function () {
+  return Rooms.find({ownerFbId: {$in: friendIds(this.userId)}});
 });
 
-Meteor.publish('listeners', function (roomId) {
-  if (!roomVisible(this.userId, roomId))
-    return [];
-  var userName = this.userId && Meteor.users.findOne(this.userId).name;
-  var listenerId = Listeners.insert({userId: this.userId, name: userName, roomId: roomId, sessionId: this.connection.id});
-  this.onStop(function () {
-    Listeners.remove(listenerId);
-  });
-  return roomId ? Listeners.find({roomId: roomId}) : Listeners.find();
+Meteor.publish('allListeners', function () {
+  return Listeners.find({fbId: {$in: friendIds(this.userId)}});
+});
+
+Meteor.publish('room', function (roomId) {
+  return roomVisible(this.userId, roomId) ? Rooms.find(roomId) : [];
 });
 
 Meteor.publish('queue', function (roomId) {
   return roomVisible(this.userId, roomId) ? Songs.find({addedTo: roomId}) : [];
+});
+
+Meteor.publish('roomListeners', function (roomId) {
+  if (!roomVisible(this.userId, roomId)) return [];
+  var user = Meteor.users.findOne(this.userId);
+  var listener = {userId: this.userId, name: user.name, fbId: user.services.facebook.id, roomId: roomId};
+  var listenerId = Listeners.insert(listener);
+  this.onStop(function () {
+    Listeners.remove(listenerId);
+  });
+  return Listeners.find({roomId: roomId});
 });
 
 Meteor.publish('favorites', function () {
