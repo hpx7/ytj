49057672209b34b1030edd8a5d7a62077dfe1952
Harsh Pandey <hpandey793@gmail.com>
2014-11-18 16:07:38 -0500

    ensure we addSong to a friend's room

---
+++ server/fb.js
@@ -0,0 +1,13 @@
+getFriendIds = function (userId) {
+  var fbInfo = Meteor.users.findOne(userId).services.facebook;
+  return _.pluck(HTTP.get('https://graph.facebook.com/v2.2/' + fbInfo.id + '/friends', {
+    params: {access_token: fbInfo.accessToken, limit: 1000}
+  }).data.data, 'id');
+};
+
+friends = function (userId, fbId) {
+  var fbInfo = Meteor.users.findOne(userId).services.facebook;
+  return HTTP.get('https://graph.facebook.com/v2.2/' + fbInfo.id + '/friends/' + fbId, {
+    params: {access_token: fbInfo.accessToken, limit: 1000}
+  }).data.data;
+}
