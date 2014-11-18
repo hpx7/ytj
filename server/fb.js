4928e2e58e912b794c662d58e5104465af1e89b4
Harsh Pandey <hpandey793@gmail.com>
2014-11-18 16:36:45 -0500

    fix fb validation

--- server/fb.js
+++ server/fb.js
@@ -9,5 +9,5 @@ friends = function (userId, fbId) {
   var fbInfo = Meteor.users.findOne(userId).services.facebook;
   return HTTP.get('https://graph.facebook.com/v2.2/' + fbInfo.id + '/friends/' + fbId, {
     params: {access_token: fbInfo.accessToken, limit: 1000}
-  }).data.data;
+  }).data.data.length;
 }
